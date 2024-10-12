adec901db8feb8bc5ff1a97f9c3dfc522bef0526
Griffin McKinley <spidergrif@gmail.com>
2024-10-11 22:40:22 -0400

    Match cases to dictionary (should be considerably faster)

--- converter.py
+++ converter.py
@@ -52,66 +52,50 @@ def letter_to_number(num):
         Returns list with each digit converted.
     """
 
+    ltn_dict = {
+    "A": "10",
+    "B": "11",
+    "C": "12",
+    "D": "13",
+    "E": "14",
+    "F": "15",
+    "G": "16",
+    "H": "17",
+    "I": "18",
+    "J": "19",
+    "K": "20",
+    "L": "21",
+    "M": "22",
+    "N": "23",
+    "O": "24",
+    "P": "25",
+    "Q": "26",
+    "R": "27",
+    "S": "28",
+    "T": "29",
+    "U": "30",
+    "V": "31",
+    "W": "32",
+    "X": "33",
+    "Y": "34",
+    "Z": "35",
+    "0": "0",
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9"
+    }
     number_list = []
-    for digit in num.lower():
+    for digit in num.upper():
         number_list.append(digit)
     finished_list = []
     for digit in number_list:
-        match digit:
-            case "a":
-                finished_list.append("10")
-            case "b":
-                finished_list.append("11")
-            case "c":
-                finished_list.append("12")
-            case "d":
-                finished_list.append("13")
-            case "e":
-                finished_list.append("14")
-            case "f":
-                finished_list.append("15")
-            case "g":
-                finished_list.append("16")
-            case "h":
-                finished_list.append("17")
-            case "i":
-                finished_list.append("18")
-            case "j":
-                finished_list.append("19")
-            case "k":
-                finished_list.append("20")
-            case "l":
-                finished_list.append("21")
-            case "m":
-                finished_list.append("22")
-            case "n":
-                finished_list.append("23")
-            case "o":
-                finished_list.append("24")
-            case "p":
-                finished_list.append("25")
-            case "q":
-                finished_list.append("26")
-            case "r":
-                finished_list.append("27")
-            case "s":
-                finished_list.append("28")
-            case "t":
-                finished_list.append("29")
-            case "u":
-                finished_list.append("30")
-            case "v":
-                finished_list.append("31")
-            case "w":
-                finished_list.append("32")
-            case "x":
-                finished_list.append("33")
-            case "y":
-                finished_list.append("34")
-            case "z":
-                finished_list.append("35")
-            case _:
-                finished_list.append(digit)
+        finished_list.append(ltn_dict[digit])
     return finished_list
 
 def number_to_letter(number_list):
@@ -129,63 +113,47 @@ def number_to_letter(number_list):
         String with letters if associated.
     """
 
+    ntl_dict = {
+        10: "A",
+        11: "B",
+        12: "C",
+        13: "D",
+        14: "E",
+        15: "F",
+        16: "G",
+        17: "H",
+        18: "I",
+        19: "J",
+        20: "K",
+        21: "L",
+        22: "M",
+        23: "N",
+        24: "O",
+        25: "P",
+        26: "Q",
+        27: "R",
+        28: "S",
+        29: "T",
+        30: "U",
+        31: "V",
+        32: "W",
+        33: "X",
+        34: "Y",
+        35: "Z",
+        0: "0",
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9"
+    }
     finished_list = []
     for digit in number_list:
-        match digit:
-            case 10:
-                finished_list.append("A")
-            case 11:
-                finished_list.append("B")
-            case 12:
-                finished_list.append("C")
-            case 13:
-                finished_list.append("D")
-            case 14:
-                finished_list.append("E")
-            case 15:
-                finished_list.append("F")
-            case 16:
-                finished_list.append("G")
-            case 17:
-                finished_list.append("H")
-            case 18:
-                finished_list.append("I")
-            case 19:
-                finished_list.append("J")
-            case 20:
-                finished_list.append("K")
-            case 21:
-                finished_list.append("L")
-            case 22:
-                finished_list.append("M")
-            case 23:
-                finished_list.append("N")
-            case 24:
-                finished_list.append("O")
-            case 25:
-                finished_list.append("P")
-            case 26:
-                finished_list.append("Q")
-            case 27:
-                finished_list.append("R")
-            case 28:
-                finished_list.append("S")
-            case 29:
-                finished_list.append("T")
-            case 30:
-                finished_list.append("U")
-            case 31:
-                finished_list.append("V")
-            case 32:
-                finished_list.append("W")
-            case 33:
-                finished_list.append("X")
-            case 34:
-                finished_list.append("Y")
-            case 35:
-                finished_list.append("Z")
-            case _:
-                finished_list.append(str(digit))
+        finished_list.append(ntl_dict[int(digit)])
     finished_string = "".join(finished_list)
     return finished_string
 
@@ -301,4 +269,5 @@ while True:
     else:
         break
 
-print(main(NUMBER, BASE, NBASE))
+print()
+print(f"The converted number is: {main(NUMBER, BASE, NBASE)}")
